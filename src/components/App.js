2ace8e25b04fc3def0eb44e180ad2433217944ff
zrincin <z.zrakoplovac@gmail.com>
2021-10-10 22:09:07 +0200

    change initial state of 'selectedVideo' to random; add footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
-
 
 class App extends Component {
   state = { videos: [], selectedVideo: null };
@@ -21,7 +20,7 @@ class App extends Component {
 
     this.setState({
       videos: response.data.items,
-      selectedVideo: response.data.items[9],
+      selectedVideo: response.data.items[Math.floor(Math.random() * 10)],
     });
   };
 
@@ -46,6 +45,18 @@ class App extends Component {
             </div>
           </div>
         </div>
+        <footer
+          style={{
+            backgroundColor: "#DDDEDE",
+            position: "fixed",
+            width: "100%",
+            left: 0,
+            bottom: 0,
+            textAlign: "center",
+          }}
+        >
+          &copy; ZrinCin, 2021.
+        </footer>
       </div>
     );
   }
